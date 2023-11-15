bc74a0acfdf06eb0c9ba35cb81b42d9dc9a9b3b0
Nisar <145307487+nisarusan@users.noreply.github.com>
2023-11-15 06:17:06 +0100

    fixing

--- notif/index.js
+++ notif/index.js
@@ -1,28 +1,21 @@
 const h4 = document.querySelector('nav h4');
 const bgElements = document.querySelectorAll('li.blue');
 const notification = document.querySelector('.notification');
-let notificationValue = 3;
-
 
 // Notification count
 for (let i = 0; i <= bgElements.length; i++) {
     notification.textContent = [i];
 }
-
 h4.addEventListener('click', () => {
     bgElements.forEach(element => {
             element.classList.remove('blue');
     });
     notification.textContent = 0;
 });
-
 bgElements.forEach(element => {
     element.addEventListener('click', (e) => {
         e.currentTarget.classList.remove('blue');
-        if(notificationValue > 0) {
-            notificationValue -= 1;
-            notification.textContent = notificationValue;
-        }
+        notification.textContent > 0 ? notification.textContent -= 1 : null;
     });
 });
 
